30e4497dace58ea4c90505010236b2e63eaf1e6c
Denis Imeri <thecrether@gmail.com>
2021-07-06 16:01:43 +0200

    Change inlude list to exclude in unzip

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -2,4 +2,4 @@
 # Get the current version
 VERSION=$(curl https://www.autohotkey.com/download/1.1/version.txt)
 curl "https://www.autohotkey.com/download/1.1/AutoHotkey_$VERSION.zip" --output AutoHotkey.zip
-unzip AutoHotkey.zip -d AutoHotkey "*.exe"
+unzip AutoHotkey.zip -d AutoHotkey -x "*.ahk"
