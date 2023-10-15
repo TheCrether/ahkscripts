0a974735fc3d2fc009606033044f2109e3c9ee91
TheCrether <thecrether@gmail.com>
2023-10-15 18:51:13 +0200

    :bug: fix typo in compile.bat #2

--- .github/scripts/compile.bat
+++ .github/scripts/compile.bat
@@ -1,5 +1,5 @@
 IF "%4%" == "" (
 	.\AutoHotkey\Compiler\Ahk2Exe.exe /in %1 /out %2 /base %3 /compress 0
 ) ELSE (
-	.\AutoHotkey\Compiler\Ahk2Exe.exe /in %1 /out %2 /base %3 /icon %4 / compress 0
+	.\AutoHotkey\Compiler\Ahk2Exe.exe /in %1 /out %2 /base %3 /icon %4 /compress 0
 )
